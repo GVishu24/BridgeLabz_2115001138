d53b1635e56032c7bd49719ba1c53b6a8270723a
GVishu24 <vishu.gupta478@gmail.com>
2025-02-14 23:37:11 +0530

    [Vishu]add. added SortingAlgo assign soln

---
+++ BubbleSort.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+
+
+//namespace SortingAlgo
+//{
+//    class BubbleSort
+//    {
+//        public static void Sort(int[] arr)
+//        {
+//            int n = arr.Length;
+//            bool swapped;
+
+//            for (int i = 0; i < n - 1; i++)
+//            {
+//                swapped = false;
+
+//                for (int j = 0; j < n - i - 1; j++)
+//                {
+//                    if (arr[j] > arr[j + 1])
+//                    {
+//                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+//                        swapped = true;
+//                    }
+//                }
+//                if (!swapped)
+//                    break;
+//            }
+//        }
+//        class Program
+//        {
+//            static void Main()
+//            {
+//                int[] arr = { 97, 85, 76, 95, 36, 55, 67, 25 };
+//                Sort(arr);
+//                Console.WriteLine("sorted marks= ");
+//                foreach (int k in arr)
+//                {
+//                    Console.Write(k+ " ");
+              
+//                } 
+//            }
+//        }
+//    }
+//}
+
+
+
---
+++ InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class InsertionSort
+{
+    static void Sort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 105, 106, 102, 201, 205, 200 };
+            Sort(arr);
+            Console.Write("sorted Id's: ");
+            foreach (int k in arr)
+            {
+                Console.Write(k+ " ");
+            }
+        }
+    }
+}
+
