ee74b48ff6cb2a16ef7b0fe3d267032bc1418b5e
GVishu24 <vishu.gupta478@gmail.com>
2025-02-14 23:34:06 +0530

    [Vishu]add. added stack/queue/hashMap & hashFunc assign soln

---
+++ TwoSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TwoSumProblem
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> numMap = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (numMap.ContainsKey(complement))
+            {
+                return new int[] { numMap[complement], i }; // Found the pair
+            }
+
+            numMap[nums[i]] = i; // Store index of current number
+        }
+
+        return new int[] { -1, -1 }; // No solution found
+    }
+
+    public static void Main()
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        int[] result = TwoSum(nums, target);
+        Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+    }
+}
