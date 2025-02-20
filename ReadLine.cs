331096217f79f9983cc51f572526fccdcb1c31d2
GVishu24 <vishu.gupta478@gmail.com>
2025-02-20 22:38:07 +0530

    [Vishu]add. added I/O Streams assignment solutions

---
+++ ReadLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class ReadLine
+{
+    static void Main()
+    {
+        string filePath = "largetext.txt"; 
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.IndexOf("error", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
+
